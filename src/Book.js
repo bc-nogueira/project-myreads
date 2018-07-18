e255c22716a6ff9c1b6e8d731984e388a8652f52
Breno Nogueira <bcnogueira@id.uff.br>
2018-07-18 17:34:25 -0300

    Refatoração: Modularizando em diferentes componentes.

---
+++ src/Book.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import BookChanger from './BookChanger'
+
+class Book extends React.Component {
+    render() {
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.url})` }}></div>
+                    <BookChanger />
+                </div>
+                <div className="book-title">{this.props.title}</div>
+                <div className="book-authors">{this.props.authors}</div>
+            </div>
+        )
+    }
+}
+
+export default Book
---
+++ src/Bookshelf.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Book from './Book'
+
+class BooksShelf extends React.Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => (
+                            <li>
+                                <Book url={book.url}
+                                    title={book.title} authors={book.authors} />
+                            </li>
+                        ))};
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BooksShelf
