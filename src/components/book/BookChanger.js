100b9e050c8da4c4268696d45c01a36fe669a5ef
bc-nogueira <bcnogueira@id.uff.br>
2018-07-26 13:08:07 -0300

    refactor: Motra qual estante está selecionada

--- src/components/book/BookChanger.js
+++ src/components/book/BookChanger.js
@@ -4,7 +4,7 @@ class BookChanger extends React.Component {
     render() {
         return(
             <div className="book-shelf-changer">
-                <select onChange={(event) => this.props.moveBook(this.props.book, event.target.value)}>
+                <select value={this.props.book.shelf} onChange={(event) => this.props.moveBook(this.props.book, event.target.value)}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
