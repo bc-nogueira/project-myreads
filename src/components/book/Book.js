4220922169778cd5fe434c36125fae5fa5641f3c
bc-nogueira <bcnogueira@id.uff.br>
2018-07-30 12:22:22 -0300

    refactor: Trata a falta de autor e da capa. Trata também quando tem mais de um autor.

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -3,15 +3,21 @@ import BookChanger from './BookChanger'
 
 class Book extends React.Component {
     render() {
+        const { book } = this.props
+
         return (
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, 
-                            backgroundImage: `url(${this.props.book.imageLinks && this.props.book.imageLinks !== undefined ? this.props.book.imageLinks.thumbnail : ''})` }}></div>
-                    <BookChanger book={this.props.book} moveBook={this.props.moveBook} />
+                            backgroundImage: `url(${book.imageLinks && book.imageLinks !== undefined ? 
+                                                    book.imageLinks.thumbnail : 
+                                                    '/default-cover.jpg'})` }}></div>
+                    <BookChanger book={book} moveBook={this.props.moveBook} />
                 </div>
-                <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors}</div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">
+                    {book.authors ? book.authors.join(", ") : 'Sem autor conhecido'}
+                </div>
             </div>
         )
     }
