14255cae7860ac203ac28dfff5f13535ac0f98a3
bc-nogueira <bcnogueira@id.uff.br>
2018-08-01 11:17:36 -0300

    Fix: Mostra livros sem prateleiras como "none"

--- src/components/book/BookChanger.js
+++ src/components/book/BookChanger.js
@@ -17,7 +17,8 @@ class BookChanger extends React.Component {
     render() {
         return(
             <div className={this.chooseBGImage()}>
-                <select value={this.props.book.shelf} onChange={(event) => this.props.moveBook(this.props.book, event.target.value)}>
+                <select value={this.props.book.shelf !== undefined ? this.props.book.shelf : 'none' }
+                        onChange={(event) => this.props.moveBook(this.props.book, event.target.value)}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
