201c723276a83369b8789589f31c31c8ea7a0256
Breno Nogueira <bcnogueira@id.uff.br>
2018-08-02 17:30:55 -0300

    refactor: Adiciona card no layout de cada prateleira

--- src/components/book/BookShelf.js
+++ src/components/book/BookShelf.js
@@ -8,25 +8,27 @@ class BooksShelf extends React.Component {
         myBooks: PropTypes.array.isRequired,
         moveBook: PropTypes.func.isRequired
     };
-    
+
     render() {
         return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.myBooks.length > 0 ?
-                            this.props.myBooks.map((book) => (
-                                <li key={book.id}>
-                                    <Book book={book} moveBook={this.props.moveBook} />
-                                </li>
-                            ))
-                            :
-                            <div className="mt-3 alert alert-warning" role="alert">
-                                <h5 className="mb-0">Não há livros nessa prateleira</h5>
-                            </div>
-                        }
-                    </ol>
+            <div className="card bookshelf m-4 pb-0">
+                <div className="card-body">
+                    <h2 className="card-title bookshelf-title">{this.props.title}</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {this.props.myBooks.length > 0 ?
+                                this.props.myBooks.map((book) => (
+                                    <li key={book.id}>
+                                        <Book book={book} moveBook={this.props.moveBook} />
+                                    </li>
+                                ))
+                                :
+                                <div className="mt-3 alert alert-warning" role="alert">
+                                    <h5 className="mb-0">Não há livros nessa prateleira</h5>
+                                </div>
+                            }
+                        </ol>
+                    </div>
                 </div>
             </div>
         );
